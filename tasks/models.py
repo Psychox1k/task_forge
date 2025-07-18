6635d896eb08306de0baea2f82cf2715729ab9e8
Psychox1k <success01001011@gmail.com>
2025-07-18 21:34:54 +0200

    feat(models): add Worker and Position models

--- tasks/models.py
+++ tasks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 class TaskType(models.Model):
@@ -61,3 +62,41 @@ class Task(models.Model):
 
     def __str__(self):
         return f"{self.name} (priority: {self.priority})"
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="workers"
+    )
+
+
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
+    @property
+    def completed_tasks(self):
+        return self.assigned_tasks.filter(is_completed=True)
+
+    @property
+    def pending_tasks(self):
+        return self.assigned_tasks.filter(is_completed=False)
+
+    def __str__(self):
+        return f"{self.username} ({self.get_full_name()})"
+
