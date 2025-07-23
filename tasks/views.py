2ef884a8c648539fc6d34c785ff434918579c208
Psychox1k <success01001011@gmail.com>
2025-07-23 21:26:52 +0200

    feat(view): show basic stats on homepage

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Team, Project, Task, Worker
 
 # Create your views here.
 def index(request):
+    num_projects = Project.objects.count()
+    num_teams = Team.objects.count()
+    num_workers = Worker.objects.count()
+
+    context = {
+        "num_projects": num_projects,
+        "num_teams": num_teams,
+        "num_workers": num_workers,
+    }
+
     return render(request, "tasks/index.html")
