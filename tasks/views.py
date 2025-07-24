d532fa637a9881b4240ec200f7f96e038bb93910
Psychox1k <success01001011@gmail.com>
2025-07-24 15:07:18 +0200

    feat(views): add generic list views for Worker, Team, Project, and Task

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Team, Project, Task, Worker
+from django.views import generic
 
 # Create your views here.
 def index(request):
@@ -14,3 +15,18 @@ def index(request):
     }
 
     return render(request, "tasks/index.html")
+
+
+class WorkerListView(generic.ListView):
+    model = Worker
+
+
+class TeamListView(generic.ListView):
+    model = Team
+
+
+class ProjectListView(generic.ListView):
+    model = Project
+
+class TaskListView(generic.ListView):
+    model = Task
