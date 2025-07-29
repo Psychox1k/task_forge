50da067ffeebd163232c65f5eb345842ddf4517f
Psychox1k <success01001011@gmail.com>
2025-07-29 16:47:08 +0200

    feat(urls): add detail views for Worker, Project, Team and Task models

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,6 +7,10 @@ from .views import (
     ProjectListView,
     TeamListView,
     TaskListView,
+    WorkerDetailView,
+    ProjectDetailView,
+    TeamDetailView,
+    TaskDetailView,
 )
 
 urlpatterns = [
@@ -14,7 +18,13 @@ urlpatterns = [
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("projects/", ProjectListView.as_view(), name="project-list"),
     path("teams/", TeamListView.as_view(), name="team-list"),
-    path("tasks/", TaskListView.as_view(), name="task-list")
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+
+    path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
+    path("projects/<int:pk>/", ProjectDetailView.as_view(), name="project-detail"),
+    path("teams/<int:pk>/", TeamDetailView.as_view(), name="team-detail"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+
 ]
 
 app_name = "task"
