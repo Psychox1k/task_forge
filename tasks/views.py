55d9261ec9e2dd2617e646179d5f2796b01f9e09
Psychox1k <success01001011@gmail.com>
2025-08-04 20:44:27 +0200

    feat(worker): add search functionality and Create button to project list

--- tasks/views.py
+++ tasks/views.py
@@ -25,6 +25,17 @@ class WorkerListView(LoginRequiredMixin, generic.ListView):
     model = Worker
     paginate_by = 2
 
+    def get_queryset(self):
+        queryset = Worker.objects.all()
+        username = self.request.GET.get("username", "")
+        if username:
+            queryset = queryset.filter(username__icontains=username)
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["username"] = self.request.GET.get("username", "")
+        return context
 
 
 class TeamListView(LoginRequiredMixin, generic.ListView):
